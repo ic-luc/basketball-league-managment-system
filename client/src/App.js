f0afbbe3a0e6dd419b215d40310201d9bb045cbe
ic-luc <flucic097@gmail.com>
2025-04-21 12:52:14 +0200

    Starting to create the front-end, made the navigation bar and the footer & the main part is in the progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logo from "./kobvlogo.svg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <a href="/"><img src={logo} alt="KOBV Logo" id="logo"/></a>
+        <nav>
+          <ul>
+            <li><a href="/meisterschaft">Meisterschaft</a></li>
+            <li><a href="/downloads">Downloads</a></li>
+            <li><a href="/verband">Verband</a></li>
+            <li><a href="/termine">Termine</a></li>
+            <li><a href="/playoffs">Playoffs</a></li>
+            <li><a href="/downloads">Schiedsrichter</a></li>
+
+          </ul>
+        </nav>
       </header>
+      <main>
+        <h1>Kärntner und Osttiroler Basketballverband</h1>
+        <div className="games">
+          <h2>Anstehende Spiele</h2>
+        </div>
+      </main>
+      <footer>
+        <p>&copy; 2023 KOBV (Created by Filip Lucic)</p>
+        <a href="/impressum">Impressum & Datenschutzerklärung</a>
+      </footer>
     </div>
   );
 }
